b0d5e4751317e89febee0317f2a64c84dd2bc035
aholic <ruochen.xu@gmail.com>
2014-10-19 13:57:54 +0800

    complete problems/powx-n/ by div-conquer

---
+++ pow-div-conquer.h
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double pow(double x, int n) {
+        if (n == 1) return x;
+        if (n == 0) return 1;
+        if (n == -1) return 1/x;
+
+        if (n % 2 == 0) {
+            double tmp = pow(x, n/2);
+            return tmp * tmp;
+        } else {
+            double tmp = pow(x, (n-1)/2);
+            return tmp * tmp * x;
+        }
+    }
+};
