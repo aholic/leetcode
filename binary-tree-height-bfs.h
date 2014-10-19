52365182df6a84e1db4a4e2617b6c76e924aff66
aholic <ruochen.xu@gmail.com>
2014-10-19 22:14:34 +0800

    complete problems/maximum-depth-of-binary-tree/ by bfs

---
+++ binary-tree-height-bfs.h
@@ -0,0 +1,34 @@
+/**
+ *   Definition for binary tree
+ *   struct TreeNode {
+ *       int val;
+ *       TreeNode *left;
+ *       TreeNode *right;
+ *       TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *   };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL) return 0;
+
+        queue<TreeNode*> q;
+        root->val = 1;
+        q.push(root);
+
+        TreeNode* tmp;
+        while (!q.empty()) {
+            tmp = q.front();
+            q.pop();
+            if (tmp->left != NULL) {
+                tmp->left->val = tmp->val + 1;
+                q.push(tmp->left);
+            }
+            if (tmp->right != NULL) {
+                tmp->right->val = tmp->val + 1;
+                q.push(tmp->right);
+            }
+        }
+        return tmp->val;
+    }
+};
