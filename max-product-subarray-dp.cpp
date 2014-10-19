558987774cc01d6d6eb8c13330c3f1746dae8bec
aholic <ruochen.xu@gmail.com>
2014-10-19 13:37:05 +0800

    complete problems/maximum-product-subarray/ by dp

---
+++ max-product-subarray-dp.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int max3(int a, int b, int c) {
+        int ret = a > b ? a : b;
+        ret = ret > c ? ret : c;
+        return ret;
+    }
+    int min3(int a, int b, int c) {
+        int ret = a < b ? a : b;
+        ret = ret < c ? ret : c;
+        return ret;
+    }
+
+    int maxProduct(int A[], int n) {
+        if (n == 0) return 0;
+
+        int *maxVal = new int [n];
+        int *minVal = new int [n];
+
+        maxVal[0] = minVal[0] = A[0];
+        for (int i = 1; i < n; i++) {
+            maxVal[i] = max3(maxVal[i-1]*A[i], A[i], minVal[i-1]*A[i]);
+            minVal[i] = min3(maxVal[i-1]*A[i], A[i], minVal[i-1]*A[i]);
+        }
+
+        int ret = maxVal[0];
+        for (int i = 1; i < n; i++) {
+            if (maxVal[i] > ret) ret = maxVal[i];
+        }
+
+        delete [] maxVal;
+        delete [] minVal;
+
+        return ret;
+    }
+};
--- sqrt-newtown.cpp
+++ sqrt-newtown.cpp
@@ -1,27 +1,15 @@
-#include <iostream>
-#include <cstdlib>
-#include <cstdio>
-
-using namespace std;
-
 class Solution {
-    public:
-        int sqrt(int x) {
-            // max answer for int x
-            double d0 = 46340;
-            double d1 = 0x7fffffff;
-            if (d0 * d0 <= x) return (int) d0;
+public:
+    int sqrt(int x) {
+        // max answer for int x
+        double d0 = 46340;
+        double d1 = 0x7fffffff;
+        if (d0 * d0 <= x) return (int) d0;
 
-            while (d1 - d0 >= 1) {
-                d1 = d0;
-                d0 = d0 / 2 + x / 2.0 / d0;
-            }
-            return (int) d0;
+        while (d1 - d0 >= 1) {
+            d1 = d0;
+            d0 = d0 / 2 + x / 2.0 / d0;
         }
+        return (int) d0;
+    }
 };
-
-int main() {
-    Solution s;
-    cout << s.sqrt(200) << endl;
-    return 0;
-}
