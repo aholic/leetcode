d941414c48b2dc3bb8333d33840933ed616a7e39
aholic <ruochen.xu@gmail.com>
2014-10-19 22:02:45 +0800

    complete problems/single-number/ by xor

---
+++ single-number-xor.h
@@ -0,0 +1,10 @@
+//https://oj.leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int ret = 0;
+        for (int i = 0 ; i < n; i++) ret ^= A[i];
+        return ret;
+    }
+};
