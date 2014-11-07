15a7e98f95001b7242fec6f75d463d03e699c3ba
aholic <ruochen.xu@gmail.com>
2014-11-07 22:09:58 +0800

    complete problems/search-insert-position/ by binary-search

---
+++ greater-or-equal.h
@@ -0,0 +1,24 @@
+// https://oj.leetcode.com/problems/search-insert-position/
+
+class Solution {
+public:
+    int searchInsert(int A[], int n, int target) {
+        int low = 0;
+        int high = n-1;
+        int mid = low + (high - low) / 2;
+
+        while (low <= high) {
+            if (A[mid] < target) {
+                low = mid + 1;
+            } else if (A[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+            mid = low + (high - low) / 2;
+        }
+        if (mid >= n) return mid;
+        if (A[mid] > target) return mid;
+        return mid+1;
+    }
+};
