584e382c4b05424f04f3e17c28de6d15ee509c5a
aholic <ruochen.xu@gmail.com>
2014-11-20 16:39:40 +0800

    improve code, it's greedy, not dp

--- buy-stock-2-greedy.h
+++ buy-stock-2-greedy.h
@@ -5,16 +5,12 @@ public:
     int maxProfit(vector<int> &prices) {
         if (prices.size() == 0) return 0;
 
-        vector<int> dp;
-        dp.resize(prices.size());
-        dp[0] = 0;
+        int ans = 0;
         for (int i = 1; i < prices.size(); i++) {
             if (prices[i] >= prices[i-1]) {
-                dp[i] = dp[i-1] + prices[i] - prices[i-1];
-            } else {
-                dp[i] = dp[i-1];
+                ans += (prices[i] - prices[i-1]);
             }
         }
-        return dp[dp.size()-1];
+        return ans;
     }
 };
