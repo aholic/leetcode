276d310af50ebed0b0aa6d270e2627de72d6ce4e
aholic <ruochen.xu@gmail.com>
2014-11-29 15:20:36 +0800

    complete problems/longest-common-prefix/ by iterate

---
+++ longest-prefix-string.h
@@ -0,0 +1,17 @@
+//https://oj.leetcode.com/problems/longest-common-prefix/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        if (strs.size() == 0) return (string());
+
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            int j = 0;
+            while (j < prefix.size() && j < strs[i].size() && strs[i][j] == prefix[j]) j++;
+            prefix = prefix.substr(0, j);
+        }
+
+        return prefix;
+    }
+};
