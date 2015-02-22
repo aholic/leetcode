dc8cb57f1a46ed2cf9416602a838bd0e69a2ab60
aholic <ruochen.xu@gmail.com>
2015-02-23 02:13:10 +0800

    complete problems/factorial-trailing-zeroes/ by math

---
+++ factorial-trailing-zeroes-math.h
@@ -0,0 +1,9 @@
+//https://oj.leetcode.com/problems/factorial-trailing-zeroes/
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if (n < 5) return 0;
+        return trailingZeroes(n/5) + n/5;
+    }
+};
