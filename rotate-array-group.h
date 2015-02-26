58a12f358a0f64db11024574609ad316c9190fbf
aholic <ruochen.xu@gmail.com>
2015-02-27 01:03:51 +0800

    complete rotate-array/ by group

---
+++ rotate-array-group.h
@@ -0,0 +1,33 @@
+//https://oj.leetcode.com/problems/rotate-array/
+
+class Solution {
+public:
+    void adjustGroup(int nums[], int n, int k, int start) {
+        int tmp = nums[start];
+        int i = start;
+
+        do {
+            nums[i] = nums[(i+n-k) % n];
+            i = (i+n-k) % n;
+        } while (i != start);
+
+        nums[start+k] = tmp;
+    }
+
+    int lcd(int a, int b) {
+        int mi = a < b ? a : b;
+        int ma = a > b ? a : b;
+        if (ma % mi == 0) return mi;
+        return lcd(mi, ma % mi);
+    }
+
+    void rotate(int nums[], int n, int k) {
+        k %= n;
+        if (n == 0 || k == 0) return;
+
+        int groupNum = lcd(n, k);
+        for (int i = 0; i < groupNum; i++) {
+            adjustGroup(nums, n, k, i);
+        }
+    }
+};
